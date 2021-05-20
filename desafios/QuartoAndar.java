1242916d04ee332ad30fbbf31a17d04f63a1db85
Jorge Luiz <jorgelsol.1902@gmail.com>
2021-05-19 22:09:55 -0300

    Update QuartoAndar.java
    Mudança no meu andar

--- desafios/QuartoAndar.java
+++ desafios/QuartoAndar.java
@@ -26,34 +26,26 @@ public class QuartoAndar {
         Random aleatorio = new Random();
         String porta1, porta2;
         String porta3;
-        int escolha1, opcaoPorta = 0;
+        int opcaoPorta = 0;
         int urna2, cc = 0;
         boolean respostaTrue = false;
         int respostaPorta = 0;
         int dado;
 
-        System.out.printf("Você chegou em um andar da torre que está muito tranquilo,\n"
-                + " mas em pouco tempo, portões irão se abrir sozinhos e alguns lobos irão\n"
-                + " se soltar e se você ainda estiver neste andar, sera devorado por eles,\n"
-                + " fuja o mais rápido possível.\n ");
+        System.out.printf("Para conseguir abrir a porta de uma só vez, você precisa mostrar que sabe desarivar a magia da porta\n"
+                + "para isso, acerte a combinação.");
 
-        System.out.println("Você deseja tentar passar as portas o mais rápido possivel ou ficar e lutar com os lobos? ");
-        System.out.printf("1- Ficar e lutar.\n2- Tentar as portas.\n");
-        escolha1 = input.nextInt();
+       
 
-        switch (escolha1) {
-            case 1:
-                System.out.println("Mensagem"); // mensagem que nao é possivel enfrentar tantos lobos.
-                break;
-            case 2:
-                long t = System.currentTimeMillis();
-                List<String> urna = Arrays.asList("62631 essa é a senha em decimal, para passar digite a mesma em octal.",
-                        "F4A7 essa é a senha em hexadecimal, para passar digite a mesma em decimal.",
-                        "0111100101000111100 essa é a senha em binario, para passar digite a mesma em octal.");
+        
+                List<String> urna = Arrays.asList("62631 essa é a senha em decimal, digite a mesma em octal para desativar a magia.",
+                        "F4A7 essa é a senha em hexadecimal, digite a mesma em decimal para desativar a magia.",
+                        "0111100101000111100 essa é a senha em binario, digite a mesma em octal para desativar a magia.");
 
                 porta1 = urna.get(0);
                 porta2 = urna.get(1);
                 porta3 = urna.get(2);
+                
                 do {
                     Collections.shuffle(urna);
                     dado = aleatorio.nextInt(2);
@@ -106,10 +98,9 @@ public class QuartoAndar {
                     }
                 } while (!respostaTrue || cc == 3);
                 if (respostaTrue) {
-                    System.out.println("Parabéns.");
-                    System.out.println("" + t);
+                    System.out.println("Você desativou a magia perfeitamente.");
                 }
         }
 
     }
-}
+
