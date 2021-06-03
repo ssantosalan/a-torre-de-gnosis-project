164aa06ac8859a0f1f30d9b4b495eca7f2d0377b
Alan Santos <ss.alan@outlook.com>
2021-06-03 15:50:35 -0300

    Update QuartoAndar.java

--- desafios/QuartoAndar.java
+++ desafios/QuartoAndar.java
@@ -22,120 +22,120 @@ public class QuartoAndar {
      * 0111100101000111100 == 745074 octal
      */
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        Random aleatorio = new Random();
-        String porta1, porta2;
-        String porta3;
-        String opcaoPorta;
-        int contadorPorta = 0;
-        int urna2, cc = 0;
-        boolean respostaTrue = false, portaTrue = false;
-        String respostaPorta;
-        int dado;
+        Scanner entrada = new Scanner(System.in);
 
-        //Digita("Você se cansa disso, seus novos poderes não são inatos e para acessá-los precisa ");
-        //Digita("querer “ativar” eles. E se lembra que junto com a sua força e magia, também adquiriu forma de desativar magias.");       
-        // if(condição do vini){    condição                                                
-        System.out.printf("Para conseguir abrir a porta de uma só vez, você precisa mostrar que sabe desativar a magia da porta\n"
-                + "para isso, acerte a combinação.\n");
+		Random aleatorio = new Random();
+		String porta1, porta2;
+		String porta3;
+		String opcaoPorta;
+		int contadorPorta = 0;
+		int cc = 0;
+		boolean respostaTrue = false, portaTrue = false;
+		String respostaPorta;
+		int dado;
 
-        List<String> urna = Arrays.asList("62631 essa é a senha em decimal, digite a mesma em octal para desativar a magia.",
-                "F4A7 essa é a senha em hexadecimal, digite a mesma em decimal para desativar a magia.",
-                "0111100101000111100 essa é a senha em binario, digite a mesma em octal para desativar a magia.");
 
-        porta1 = urna.get(0);
-        porta2 = urna.get(1);
-        porta3 = urna.get(2);
+		System.out.println();
+		System.out.println(
+				"Para conseguir abrir a porta de uma só vez, você precisa mostrar que sabe desativar a magia da porta.");
+		System.out.println("Para isso, acerte a combinação.");
+		System.out.println();
 
-        do {
-            Collections.shuffle(urna);
-            dado = aleatorio.nextInt(2);
-            System.out.printf("Porta 1 - \nPorta 2 - \nPorta 3 - \n"); // mostrar opções de porta
+		List<String> urna = Arrays.asList(
+				"62631 essa é a senha em decimal. Digite a mesma em octal para desativar a magia.",
+				"F4A7 essa é a senha em hexadecimal. Digite a mesma em decimal para desativar a magia.",
+				"0111100101000111100 essa é a senha em binario. Digite a mesma em octal para desativar a magia.");
 
-            System.out.println("Escolha uma das 3 portas e digite a senha correta.");  // escolher porta
-            opcaoPorta = input.next();
-            
-            
+		porta1 = urna.get(0);
+		porta2 = urna.get(1);
+		porta3 = urna.get(2);
 
-            
-                switch (opcaoPorta) {
-                    case "1":
-                        portaTrue = true;
-                        break;
-                    case "2":
-                        portaTrue = true;
-                        break;
-                    case "3":
-                        portaTrue = true;
-                        break;
-                    default:
-                        System.out.println("Escolha portas de 1 até 3. ");
-                        contadorPorta++;
-                        break;
-                }
-            
+		do {
+			Collections.shuffle(urna);
+			dado = aleatorio.nextInt(2);
+			System.out.printf("Porta 1 - \nPorta 2 - \nPorta 3 - \n"); // mostrar opções de porta
 
-        } while (portaTrue == false && contadorPorta <3);
-        
-        if(portaTrue == true){
-        do {
-            System.out.println("Você escolheu a porta " + opcaoPorta); // porta escolhida
-            System.out.println("" + urna.get(dado));
-            System.out.println("Qual a senha correta para abrir esta porta? "); // pedir senha da porta escolhida
-            respostaPorta = input.next();
+			System.out.print("Escolha uma das 3 portas e digite a senha correta: "); // escolher porta
+			opcaoPorta = entrada.next();
 
-            switch (respostaPorta) {
-                case "172247":
-                    if (urna.get(dado).equals(porta1)) {
-                        System.out.println("Resposta certa.");
-                        respostaTrue = true;
+			switch (opcaoPorta) {
+			case "1":
+				portaTrue = true;
+				break;
+			case "2":
+				portaTrue = true;
+				break;
+			case "3":
+				portaTrue = true;
+				break;
+			default:
+				System.out.println("Escolha portas de 1 até 3. ");
+				contadorPorta++;
+				break;
+			}
 
-                    } else {
-                        System.out.println("Resposta errada.");
-                        cc++;
-                    }
-                    break;
-                case "62631":
-                    if (urna.get(dado).equals(porta2)) {
-                        System.out.println("Certo.");
-                        respostaTrue = true;
+		} while (portaTrue == false && contadorPorta < 3);
+		
+		System.out.println();
+		if (portaTrue == false) {
+			System.out.println("Indeciso... Até mesmo escolher uma porta é difícil para você... ");
+			System.out.println("Fim de jogo.");
+			System.exit(0);
+		} 
 
-                    } else {
-                        System.out.println("Errado.");
-                        cc++;
-                    }
-                    break;
-                case "745074":
-                    if (urna.get(dado).equals(porta3)) {
-                        System.out.println("Certa.");
-                        respostaTrue = true;
+		do {
+			System.out.println("Você escolheu a porta " + opcaoPorta); // porta escolhida
+			System.out.println("" + urna.get(dado));
+			System.out.println("Informe a senha para abrir a porta: "); // pedir senha da porta escolhida
+			respostaPorta = entrada.next();
 
-                    } else {
-                        System.out.println("errada.");
-                        cc++;
-                    }
-                    break;
-                default:
-                    System.out.println("Resposta errada.");
-            }
-            if (cc == 3) {
-                System.out.println("Voce errou demais.");
-            }
-        } while (!respostaTrue || cc == 3);
-        //}else if (outra condicao){
-        
-        //}   parenteses do else if
-        if (respostaTrue) {
-            System.out.println("Você desativou a magia perfeitamente.");
-        }
-        }else{
-            System.out.println("Você não aprendeu o suficiente para desativar a magia. :( ");
-        }
-        //Digita("Asulf se concentra e busca em seu âmago toda força que seu pacto lhe conferiu e");
-        //Digita("puxou a porta de uma só vez, ele destrói a porta e a água  começa a jorrar no");
-        //Digita("ambiente. O  resto de oxigênio abandona a sala e corre em direção à superfície, a ");
-        //Digita("água rapidamente inunda a sala e você logo se encontra submerso.");
+			switch (respostaPorta) {
+			case "172247":
+				if (urna.get(dado).equals(porta1)) {
+					System.out.println("Resposta certa!");
+					respostaTrue = true;
 
-    }
+				} else {
+					System.out.println("Resposta errada!");
+					cc++;
+				}
+				break;
+			case "62631":
+				if (urna.get(dado).equals(porta2)) {
+					System.out.println("Certo!");
+					respostaTrue = true;
+
+				} else {
+					System.out.println("Errado!");
+					cc++;
+				}
+				break;
+			case "745074":
+				if (urna.get(dado).equals(porta3)) {
+					System.out.println("Certa!");
+					respostaTrue = true;
+
+				} else {
+					System.out.println("Errada!");
+					cc++;
+				}
+				break;
+			default:
+				System.out.println("Resposta errada!");
+				cc++;
+			}
+			if (cc == 3) {
+				System.out.println("Você errou demais!");
+				System.exit(0);
+			}
+		} while (!respostaTrue || cc == 3);
+
+		if (respostaTrue) {
+			System.out.println("Você desativou a magia perfeitamente!");
+		}
+
+		System.out.println("TO BE CONTINUED");
+
+	}
 
 }
